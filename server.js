83eabcca7ceff8c1ab45543d936ffa20d0e5fec7
josephditta56 <josephditta56@gmail.com>
2025-11-28 09:33:32 -0500

    server.js

--- server.js
+++ server.js
@@ -5,18 +5,37 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.use('/proxy', (req, res, next) => {
-  const target = req.query.url;
+app.use('/proxy', createProxyMiddleware({
+  target: '', // dynamic target below
+  changeOrigin: true,
+  secure: false,
+  selfHandleResponse: true, // we'll handle the response
+  onProxyReq: (proxyReq, req, res) => {
+    // dynamically set target from query param
+    proxyReq.setHeader('host', new URL(req.query.url).host);
+  },
+  onProxyRes: async (proxyRes, req, res) => {
+    let body = [];
+    proxyRes.on('data', (chunk) => body.push(chunk));
+    proxyRes.on('end', () => {
+      body = Buffer.concat(body).toString('utf8');
 
-  if (!target) return res.status(400).send('No URL specified');
+      // Remove X-Frame-Options and CSP headers
+      proxyRes.headers['x-frame-options'] = '';
+      proxyRes.headers['content-security-policy'] = '';
 
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: { '^/proxy': '' },
-    secure: false
-  })(req, res, next);
-});
+      // Send the response to the browser
+      res.set(proxyRes.headers);
+      res.send(body);
+    });
+  },
+  pathRewrite: {
+    '^/proxy': '',
+  },
+  router: (req) => {
+    return req.query.url; // dynamically route to the URL in query param
+  }
+}));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Proxy running on port ' + PORT));
